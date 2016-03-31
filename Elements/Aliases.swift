5bbd77dddce4affe9d3fcbdddc8aaece53123874
marc hoffman <mh@remobjects.com>
2016-03-31 10:45:04 -0400

    Aliases & Darwin stuff

---
+++ Elements/Aliases.swift
@@ -0,0 +1,9 @@
+﻿import Foundation
+
+public typealias timeval = __struct_timeval
+public typealias timespec = __struct_timespec
+public typealias tm = __struct_tm
+
+public typealias stat = __struct_stat
+//public typealias statfs = __struct_statfs
+
